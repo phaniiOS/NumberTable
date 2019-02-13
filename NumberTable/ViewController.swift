6e847799d0b957adb74cc40a05ab4e686c8342ac
Phani <phanindranath.wg113@gmail.com>
2019-02-12 22:12:13 -0600

    Initial Commit

--- NumberTable/ViewController.swift
+++ NumberTable/ViewController.swift
@@ -8,8 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let it: Int = 50
+    var num: Int = 0
+    @IBOutlet weak var SliderView: UILabel!
+    @IBOutlet weak var TableViewOutlet: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return it
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let NumTabCell = tableView.dequeueReusableCell(withIdentifier: "NumTabCell", for: indexPath) as UITableViewCell
+        while indexPath.row <= it{
+            NumTabCell.textLabel?.text = "\(num) * \(indexPath.row + 1) = \(num * (indexPath.row + 1))"
+            return NumTabCell
+        }
+        return NumTabCell
+    }
+    
+    @IBAction func Slider(_ sender: UISlider) {
+        num = Int(sender.value)
+        SliderView.text = "The number table for \(num) is displayed below"
+        TableViewOutlet.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
